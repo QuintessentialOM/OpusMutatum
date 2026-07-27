b8671dda86a908dda06360dac6c72fb0de308180
aonkeeper4 <aonkeeper4@gmail.com>
2026-07-27 15:36:14 +0100

    kill hookgen

--- Patching.cs
+++ Patching.cs
@@ -7,15 +7,12 @@ namespace OpusMutatum;
 
 public static class Patching {
     public static string PathToMonoMod = "MonoMod.Patcher.dll";
-    public static string PathToHookGen = "MonoMod.RuntimeDetour.HookGen.dll";
-    public static string PathToPatchingDependencies = ".";
+    public static string PathToPatchingDependencies = "";
 
     public static string PathToQuintessential = "Quintessential.dll";
 
     private static bool TryLoadMonoMod(out Assembly monoModAssembly)
         => Globals.TryLoadAssembly(PathToMonoMod, out monoModAssembly);
-    private static bool TryLoadHookGen(out Assembly monoModAssembly)
-        => Globals.TryLoadAssembly(PathToHookGen, out monoModAssembly);
 
     public static void RunMonoMod(string asmFrom, string asmTo = null, string[] dllPaths = null) {
         if (!TryLoadMonoMod(out Assembly monoModAssembly)) {
@@ -39,29 +36,11 @@ public static class Patching {
 
             if (!File.Exists(asmTmp))
                 throw new Exception($"MonoMod failed to create a patched assembly: exit code {returnCode}!");
-
             File.Move(asmTmp, asmTo);
         } finally {
             File.Delete(asmTmp);
             File.Delete(Path.ChangeExtension(asmTmp, "pdb"));
             File.Delete(Path.ChangeExtension(asmTmp, "mdb"));
         }
-    }
-
-    public static void RunHookGen(string asmFrom, string asmTo = null) {
-        if (!TryLoadHookGen(out Assembly hookGenAssembly)) {
-            Console.WriteLine("Unable to load MonoMod HookGen, skipping patching!");
-            return;
-        }
-
-        asmTo ??= asmFrom;
-
-        Console.WriteLine($"Running MonoMod HookGen for {asmFrom}...");
-
-        Environment.SetEnvironmentVariable("MONOMOD_DEPDIRS", PathToPatchingDependencies);
-        Environment.SetEnvironmentVariable("MONOMOD_DEPENDENCY_MISSING_THROW", "0");
-
-        string mmHookAssemblyName = "MMHOOK_" + Path.ChangeExtension(Path.GetFileName(asmTo), "dll");
-        hookGenAssembly.EntryPoint!.Invoke(null, [ new[] { "--private", asmFrom, Path.Combine(Path.GetDirectoryName(asmTo)!, mmHookAssemblyName) }]);
     }
 }

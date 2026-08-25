c42273834da174079fb6396a172cefbeb9e5597a
Fate of Adrastus <203406394+Fate-of-Adrastus@users.noreply.github.com>
2026-08-26 00:00:37 +0200

    Hotfix2

--- Merging/CodeExecutionManager.cs
+++ Merging/CodeExecutionManager.cs
@@ -171,7 +171,6 @@ public class CodeExecutionManager {
                 Runtime = TargetRuntime.Net_2_0
             }
         );
-        wrapper.PatchTargetArchitecture();
         var wrapperMod = new MonoModCodeRulesModder() {
             Module = wrapper,
             Orig = orig,
